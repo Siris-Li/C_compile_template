f315f5f42037408827c32c3305e2e972624ce7b5
Siris Li <mingxuanli_siris@163.com>
2024-12-27 17:54:36 +0800

    Add script to extract hex instructions from disassembly

--- Makefile
+++ Makefile
@@ -3,7 +3,8 @@ RISCV_OBJDUMP?=riscv-none-elf-objdump
 
 all:
 	$(RISCV_GCC) -mcmodel=medany -Wall -mexplicit-relocs -march=rv64im -mabi=lp64 -nostdlib -static -Tlinker.ld -ggdb -Wl,--no-gc-sections test_code.c -o test_code.o
-	$(RISCV_OBJDUMP) -d test_code.o > test_code.d
+	$(RISCV_OBJDUMP) -D -s test_code.o > test_code.asm
+	python asm2hex.py
 
 clean:
-	rm test_code.o test_code.d
+	rm test_code.o test_code.asm init_mem.hex
---
+++ asm2hex.py
@@ -0,0 +1,46 @@
+data_sections = ['.text', '.rodata']
+data = {'.text': [], '.rodata': []}
+current_section = None
+
+def big_to_little_endian(hex_str):
+    # 将大端表示的16进制字符串转换为小端表示的16进制字符串
+    byte_array = bytearray.fromhex(hex_str)
+    byte_array.reverse()
+    return ''.join(format(x, '02x') for x in byte_array)
+
+with open('test_code.asm', 'r') as f:
+    for line in f:
+        line = line.strip()
+        # 检查是否进入新的段
+        if line.startswith('Contents of section'):
+            for sec in data_sections:
+                if line.endswith(f'{sec}:'):
+                    current_section = sec
+                    break
+            else:
+                current_section = None
+        # 如果在目标段内，处理数据行
+        elif current_section:
+            if line:
+                parts = line.split()
+                if len(parts) >= 5:
+                    data_values = [big_to_little_endian(val) for val in parts[1:5] if all(c in '0123456789abcdefABCDEF' for c in val)]  # 提取并转换4个数据
+                    data[current_section].append(data_values)
+                elif len(parts) > 1:
+                    address = parts[0]
+                    data_values = [big_to_little_endian(val) for val in parts[1:] if all(c in '0123456789abcdefABCDEF' for c in val)]  # 提取并转换所有数据
+                    data[current_section].append(data_values)
+            else:
+                # 空行表示段的结束
+                current_section = None
+
+# 将数据两两配对并输出到文件
+with open('init_mem.hex', 'w') as output_file:
+    for section in data_sections:
+        for data_values in data[section]:
+            # 检查data_values的数目是否为偶数，如果不是，补齐0
+            if len(data_values) % 2 != 0:
+                data_values.append('00000000')
+            for i in range(0, len(data_values), 2):
+                paired_value = data_values[i+1] + data_values[i]
+                output_file.write(paired_value + '\n')
