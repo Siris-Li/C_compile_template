ecd7f3540cd187c501a694f105514632c3cdf67a
Bwoah-Kimi <jameszhu.20021122@gmail.com>
2025-02-19 16:24:27 +0800

    Add DCO regs control C code.

--- src/test_code.c
+++ src/test_code.c
@@ -1,26 +1,24 @@
 #include <stdint.h>
 
-void func()
-{
-    // inline assembly code
-    __asm__ volatile(
-        "li t0, 1;"             // load "1" to register t0
-        "li t1, 2;"             // load "2" to register t1
-        "li t2, 0x80001000;"    // load "0x80001000" to register t2
+void func() {
+  // inline assembly code
+  __asm__ volatile(
+      "li t0, 1;"           // load "1" to register t0
+      "li t1, 2;"           // load "2" to register t1
+      "li t2, 0x80001000;"  // load "0x80001000" to register t2
 
-        "add t3, t0, t1;"       // add t0 and t1, store the result in t3
-        "sub t4, t0, t1;"       // sub t0 and t1, store the result in t4
+      "add t3, t0, t1;"  // add t0 and t1, store the result in t3
+      "sub t4, t0, t1;"  // sub t0 and t1, store the result in t4
 
-        "sw t3, 0(t2);"         // store the value in t3 to the address in t2
-        "sw t4, 4(t2);"         // store the value in t4 to the address in t2 + 4
+      "sw t3, 0(t2);"  // store the value in t3 to the address in t2
+      "sw t4, 4(t2);"  // store the value in t4 to the address in t2 + 4
 
-        "lw t5, 0(t2);"         // load the value in the address in t2 to t5
-        "lw t6, 4(t2);"         // load the value in the address in t2 + 4 to t6
-    );
+      "lw t5, 0(t2);"  // load the value in the address in t2 to t5
+      "lw t6, 4(t2);"  // load the value in the address in t2 + 4 to t6
+  );
 
-    // normal C code
-    uint64_t *mem_base = (uint64_t *)0x80001000;
-    *(mem_base + 0) = (uint64_t)0xb6acad2abb260109;
-    *(mem_base + 1) = (uint64_t)0x11752c63ab69c863;
-
+  // normal C code
+  uint64_t* mem_base = (uint64_t*)0x80001000;
+  *(mem_base + 0) = (uint64_t)0xb6acad2abb260109;
+  *(mem_base + 1) = (uint64_t)0x11752c63ab69c863;
 }
